69ea271a3138b38193dfa0ccba3a649520be8195
aianafitlife <145410386+aianafitlife@users.noreply.github.com>
2023-11-03 20:07:51 +0600

    fixed styles in cirlce component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import logo from './assets/logo.png'
+import TextAreaInput from './components/TextArea/TextArea';
+import {useState} from "react";
 import Bubbles from './components/Bubbles/Bubble';
+import Circle from "./components/Circle/Circle";
 
 function App() {
+  const [value, setValue] = useState('Text something')
+
   return (
     <div className="App">
-      <Bubbles/>
+      <img className='logo' src={logo} alt="/"/>
+      <Circle text={value}/>
+      <TextAreaInput setValue={setValue}/>
+      <Bubbles text={value}/>
     </div>
   );
 }
